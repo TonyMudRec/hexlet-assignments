1d52511217e3da870b7cf545830a821e27a01604
@hexlet/cli <support@hexlet.io>
2023-03-28 11:05:32 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -6,10 +6,14 @@ import java.util.Map;
 // BEGIN
 class App {
     public static void main(String[] args) {
-        String sentence = "the java is the best programming language java";
-        Map wordsCount = App.getWordCount(sentence);
-        String result = App.toString(wordsCount);
-        System.out.println(result);
+        String sentence2 = "the java is the best programming language java";
+        Map wordsCount2 = App.getWordCount(sentence2);
+        String result2 = App.toString(wordsCount2);
+        System.out.println(result2); // =>
+
+        Map wordsCount3 = App.getWordCount("");
+        String result3 = App.toString(wordsCount3);
+        System.out.println(result3); // => {}
     }
 
     public static Map<String, Integer> getWordCount(String sentence) {
@@ -20,7 +24,7 @@ class App {
             if (result.containsKey(word)) {
                 tmp = result.get(word);
                 result.put(word, tmp + 1);
-            } else {
+            } else if (!word.isEmpty()) {
                 result.put(word, 1);
             }
         }
@@ -28,8 +32,7 @@ class App {
     }
 
     protected static <S, I> String toString(Map<S, I> sentence) {
-        StringBuilder sb = new StringBuilder("{");
-        sb.append("\n");
+        StringBuilder sb = new StringBuilder("{\n");
         for (Map.Entry<S, I> entry : sentence.entrySet()) {
             sb.append(String.format("  %s: %1d\n", entry.getKey(), entry.getValue()));
         }
