67a406c7b7125b73f3f590a8b2a3cfeb5723ef4c
@hexlet/cli <support@hexlet.io>
2023-10-26 15:16:15 +0300

    submit java-spring-ru/one-to-many

---
+++ java-spring-ru/one-to-many/src/main/java/exercise/controller/TasksController.java
@@ -0,0 +1,84 @@
+package exercise.controller;
+
+import java.util.List;
+
+import exercise.dto.TaskCreateDTO;
+import exercise.dto.TaskDTO;
+import exercise.dto.TaskUpdateDTO;
+import exercise.mapper.TaskMapper;
+import exercise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import exercise.exception.ResourceNotFoundException;
+import exercise.repository.TaskRepository;
+
+@RestController
+@RequestMapping("/tasks")
+public class TasksController {
+    // BEGIN
+    @Autowired
+    private TaskRepository tRepo;
+
+    @Autowired
+    private UserRepository uRepo;
+
+    @Autowired
+    private TaskMapper mapper;
+
+
+    @GetMapping("")
+    public List<TaskDTO> index() {
+        return tRepo.findAll().stream().map(t -> mapper.map(t)).toList();
+    }
+
+    @GetMapping("/{id}")
+    public TaskDTO show(@PathVariable Long id) {
+        var task = tRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Task with id %d not exist", id)));
+        var taskDTO = mapper.map(task);
+        taskDTO.setAssigneeId(task.getAssignee().getId());
+        return taskDTO;
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskDTO create(@RequestBody TaskCreateDTO taskCreateDTO) {
+//        var id = taskCreateDTO.getAssigneeId();
+//        var user = uRepo.findById(id)
+//                .orElseThrow(() -> new ResourceNotFoundException(String.format("User with id %d not exist", id)));
+        var task = mapper.map(taskCreateDTO);
+//        task.setAssignee(user);
+        tRepo.save(task);
+        var taskDTO = mapper.map(task);
+//        taskDTO.setAssigneeId(id);
+        return taskDTO;
+    }
+
+    @PutMapping("/{id}")
+    public TaskDTO update(@PathVariable Long id, @RequestBody TaskUpdateDTO updateDTO) {
+        var task = tRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Task with id %d not exist", id)));
+        mapper.update(updateDTO, task);
+        tRepo.save(task);
+        return mapper.map(task);
+    }
+
+    @DeleteMapping("")
+    public void delete(@PathVariable Long id) {
+        var task = tRepo.findById(id).get();
+        var user = task.getAssignee();
+        user.removeTask(task);
+        tRepo.deleteById(id);
+    }
+    // END
+}
---
+++ java-spring-ru/one-to-many/src/main/java/exercise/mapper/TaskMapper.java
@@ -0,0 +1,28 @@
+package exercise.mapper;
+
+import exercise.dto.TaskCreateDTO;
+import exercise.dto.TaskDTO;
+import exercise.dto.TaskUpdateDTO;
+import exercise.model.Task;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class TaskMapper {
+
+    // BEGIN
+    public abstract Task map(TaskCreateDTO createDTO);
+    public abstract TaskDTO map(Task task);
+    public abstract void update(TaskUpdateDTO updateDTO, @MappingTarget Task model);
+
+    // END
+
+}
